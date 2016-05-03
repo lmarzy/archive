7dcf6e64089f1227b6310016b257595cdc3f2f69
Lee Marsden <marzy@Lees-MacBook-Pro.local>
2016-05-03 13:59:26 +0100

    Added latest

---
+++ gulp/config/build.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  a: 'nunjucks',
+  b: [
+    'styles',
+    'js',
+    'assets:css',
+    'assets:images',
+    'htmlValidation',
+  ],
+  c: [
+    'revAll',
+    'sitemap'
+  ]
+}
